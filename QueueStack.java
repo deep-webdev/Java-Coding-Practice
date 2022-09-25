7d67f326b4ca0cf0f9aabecde42a87766397273d
Deep Panchal <deeppanchal2410@gmail.com>
2022-09-25 21:49:33 +0530

    build tree and traverse tree in different ways

---
+++ QueueStack.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class QueueStack {
+	
+	static class Queue{
+		static Stack<Integer> s1 = new Stack<>();
+		static Stack<Integer> s2 = new Stack<>();
+		
+		public static boolean isEmpty() {
+			return s1.isEmpty();
+		}
+	}
+	public static void main(String args[]) {
+		
+	}
+}
